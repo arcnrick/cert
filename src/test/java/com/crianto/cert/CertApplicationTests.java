3b0e25ff0758229647ce71055f6e8c1bb7f27279
Arcn Rick <arcnrick@gmail.com>
2022-08-18 20:17:05 -0300

    Adicionando arquivos pós criação do projeto

---
+++ src/test/java/com/crianto/cert/CertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crianto.cert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
