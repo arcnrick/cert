ccea800291d7bb9e2c0b2881f1566769687c88a3
Arcn Rick <arcnrick@gmail.com>
2022-08-24 20:53:58 -0300

    Adicionado controller e security config

--- src/main/java/com/crianto/cert/CertApplication.java
+++ src/main/java/com/crianto/cert/CertApplication.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@PropertySource("classpath:application.properties")
 public class CertApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CertApplication.class, args);
-	}
+    public static void main(String[] args) {
+        //SpringApplication.run(CertApplication.class, args);
+
+		SpringApplication application = new SpringApplication(CertApplication.class);
+		application.setAdditionalProfiles("ssl");
+		application.run(args);
+    }
 
 }
--- src/main/java/com/crianto/cert/controller/HelloController.java
+++ src/main/java/com/crianto/cert/controller/HelloController.java
@@ -5,18 +5,21 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class HelloController {
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello() {
-        return "Hello Brow";
+    @RequestMapping(value = "/welcome", method = RequestMethod.GET)
+    public String welcome() {
+        System.out.println("*** welcome ***");
+        return "Welcome Brow";
     }
 
     @RequestMapping(value = "/hello-people/{people}", method = RequestMethod.GET)
     public String helloPeople(@PathVariable("people") String people) {
+        System.out.println("*** hello-people ***");
         return "Hello " + people;
     }
 
     @RequestMapping(value = "/hello-param", method = RequestMethod.GET)
     public String helloParam(@RequestParam("param") String param) {
+        System.out.println("*** hello-param ***");
         return "Hello " + param;
     }
 
